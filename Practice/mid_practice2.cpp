d802c6eac75b03af006d7393f46136d4d78cd9f4
SamiKhan-cse19 <samikhan@iut-dhaka.edu>
2021-06-20 16:21:16 +0600

    practice 2 editted

--- Practice/mid_practice2.cpp
+++ Practice/mid_practice2.cpp
@@ -15,9 +15,15 @@ String() //no-arg constructor
 String( char s[] ) //1-arg constructor
 { strcpy(str, s); } // convert C-string to String
 void display() const //display the String
-{ cout << str; }
+{ cout << str << endl; }
 operator char*() //conversion operator
 { return str; } //convert String to C-string
+String operator + (String s2) {
+    return strcat(str, s2.str);
+}
+void display2(String s2) {
+    cout<<(operator +(s2)).str<<endl;
+}
 };
 ////////////////////////////////////////////////////////////////
 int main()
@@ -29,6 +35,7 @@ s1 = xstr; //use 1-arg constructor
 // to convert C-string to String
 s1.display(); //display String
 String s2 = "Bonne Annee!"; //uses 1-arg constructor
+s1.display2(s2);
 //to initialize String
 cout << static_cast<char*>(s2); //use conversion operator
 cout << endl; //to convert String to C-string
